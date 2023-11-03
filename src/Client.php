da85916978f6d297368f27c6c2fe09e04bc7305d
Tom Lawton <tom.lawton@cndu.it>
2023-11-03 17:17:07 +0000

    feature: add basic auth method

--- src/Client.php
+++ src/Client.php
@@ -9,9 +9,28 @@ class Client
 {
     private string $baseUrl;
 
+    private ?array $basicAuth;
+
     public function __construct(string $baseUrl = null)
     {
         $this->baseUrl = $baseUrl ?? strval(config('wordpress-api.url'));
+        $this->basicAuth = null;
+    }
+
+    /**
+     * @param ?string $user_or_token
+     * @param ?string $password
+     * @return void
+     */
+    public function auth(?string $user_or_token = null, ?string $password = null): void
+    {
+        if(null === $user_or_token) {
+            $this->basicAuth = null;
+        } else if(!isset($password)) {
+            $this->basicAuth = explode(':', $user_or_token);
+        } else {
+            $this->basicAuth = [$user_or_token, $password];
+        }
     }
 
     /**
@@ -22,6 +41,11 @@ class Client
      */
     public function send(string $method, string $endpoint, array $options = []): Response
     {
+
+        if(is_array($this->basicAuth) && count($this->basicAuth) === 2) {
+            return Http::withBasicAuth($this->basicAuth[0], $this->basicAuth[1])->send($method, $this->baseUrl.$endpoint, $options);
+        }
+
         return Http::send($method, $this->baseUrl.$endpoint, $options);
     }
 }
